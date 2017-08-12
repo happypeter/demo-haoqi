d91da5512813665122f5601d39d89e441fe267b5
Peter Wang <happypeter1983@gmail.com>
2017-08-12 08:49:30 +0800

    2-comp

---
+++ src/components/Post.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="post">
+    <div class="upper">
+      <post-body></post-body>
+    </div>
+    <div class="bottom">
+      <comment-box></comment-box>
+    </div>
+  </div>
+</template>
+
+<script>
+  import PostBody from './PostBody'
+  import CommentBox from './CommentBox'
+  export default {
+    components: { PostBody, CommentBox }
+  }
+</script>
+
+<style scoped>
+  .post {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+  .upper {
+    background-color: #00bcd4;
+    padding-top: 40px;
+    padding-bottom: 40px;
+  }
+
+  .bottom {
+    background-color: #f4f3f3;
+    flex-grow: 1;
+    padding-top: 40px;
+  }
+
+</style>
---
+++ src/components/PostBody.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="post-body">
+    PostBody
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'post-body'
+  }
+</script>
+
+<style scoped>
+  .post-body {
+    height: 150px;
+    width: 80%;
+    margin: 0 auto;
+    background-color: #fff;
+    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.5);
+    text-align: center;
+    color: rgba(0, 0, 0, 0.8);
+    padding: 10px;
+  }
+</style>
