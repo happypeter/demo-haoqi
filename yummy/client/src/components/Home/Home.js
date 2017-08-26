056ba5e82f276a745f5a91c34c50841fe2d3c525
Peter Wang <happypeter1983@gmail.com>
2017-08-26 11:33:05 +0800

    home hero

--- yummy/client/src/components/Home/Home.js
+++ yummy/client/src/components/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import './home.css'
+import logo from './logo.svg'
 
 export default () => (
   <div className='home'>
-    <div className='home-hero'>Hero</div>
+    <div className='home-hero'>
+      <img src={logo} alt='logo' className='home-logo' />
+      <div className='home-title'>
+        吮指
+      </div>
+      <div className='home-slogan'>
+        享受舌尖艳遇
+      </div>
+    </div>
     <div className='home-actions'>Actions</div>
   </div>
 )
