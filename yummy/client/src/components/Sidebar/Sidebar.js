1fdc802d539c34f8793e7836a714e8ed525df7bd
Peter Wang <happypeter1983@gmail.com>
2017-09-01 21:24:01 +0800

    sidebar done

--- yummy/client/src/components/Sidebar/Sidebar.js
+++ yummy/client/src/components/Sidebar/Sidebar.js
@@ -3,12 +3,50 @@ import { slide as Menu } from 'react-burger-menu'
 import { Link } from 'react-router-dom'
 import './sidebar.css'
 
-
-export default () => (
-  <div className='sidebar'>
-    <Menu>
-      <Link className='menu-item' to='/'>首页</Link>
-      <Link className='menu-item' to='/login'>登录</Link>
-    </Menu>
+let userInfoText = (
+  <div className='sidebar-user-text'>
+    <Link to='#' className='sidebar-user-name'>
+      happypeter
+    </Link>
+    <Link to='#'>退出</Link>
   </div>
 )
+
+export default class Sidebar extends React.Component {
+  state = {
+    isOpen: false
+  }
+
+  closeMenu = () => {
+    this.setState({
+      isOpen: false
+    })
+  }
+  render () {
+    return (
+      <div className='sidebar'>
+        <Menu isOpen={this.state.isOpen}
+          customCrossIcon={false}
+          >
+          <div className='sidebar-user-info'>
+            <div className='sidebar-avatar' />
+            {userInfoText}
+          </div>
+          <div className='bm-link-list'>
+            <Link
+              onClick={this.closeMenu}
+              className='menu-item' to='/'>首页</Link>
+            <Link
+              onClick={this.closeMenu}
+              className='menu-item' to='/login'>登录</Link>
+          </div>
+          <div className='bottom-button'>
+            <button
+              onClick={this.closeMenu}
+              className='bm-close-button' >关闭</button>
+          </div>
+        </Menu>
+      </div>
+    )
+  }
+}
