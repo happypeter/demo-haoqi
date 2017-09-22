9e4799d84af9dc739791a5ec5c6f15b942e2da00
Peter Wang <happypeter1983@gmail.com>
2017-09-22 21:19:39 +0800

    dog-5-axios

--- react-baby-v2/ten-dogs/src/components/Home/Home.js
+++ react-baby-v2/ten-dogs/src/components/Home/Home.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
 import './home.css'
 import DogIcon from '../DogIcon/DogIcon'
+import axios from 'axios'
 
 class Home extends Component {
   state = {
     dogs: []
   }
+
+  componentDidMount () {
+    axios.get('http://localhost:3008/dogs').then( res => {
+      this.setState({
+        dogs: res.data
+      })
+    })
+  }
+
   render () {
     const dogList = this.state.dogs.map(t => (
       <div key={t.id} className='dog-icon-wrap'>
