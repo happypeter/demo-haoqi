c76fd7e888b9254c8eb595893ab8af416d5145e8
Peter Wang <happypeter1983@gmail.com>
2017-09-22 20:55:05 +0800

    dog-4-api

--- react-baby-v2/ten-dogs/src/components/Home/Home.js
+++ react-baby-v2/ten-dogs/src/components/Home/Home.js
@@ -4,32 +4,7 @@ import DogIcon from '../DogIcon/DogIcon'
 
 class Home extends Component {
   state = {
-    dogs: [
-      {
-        id: 1,
-        imgUrl: 'https://github.com/happypeter/demo-haoqi/blob/master/react-baby-v2/ten-dogs/img/dog1.jpg?raw=true'
-      },
-      {
-        id: 2,
-        imgUrl: 'https://github.com/happypeter/demo-haoqi/blob/master/react-baby-v2/ten-dogs/img/dog2.jpg?raw=true'
-      },
-      {
-        id: 3,
-        imgUrl: 'https://github.com/happypeter/demo-haoqi/blob/master/react-baby-v2/ten-dogs/img/dog3.jpg?raw=true'
-      },
-      {
-        id: 4,
-        imgUrl: 'https://github.com/happypeter/demo-haoqi/blob/master/react-baby-v2/ten-dogs/img/dog4.jpg?raw=true'
-      },
-      {
-        id: 5,
-        imgUrl: 'https://github.com/happypeter/demo-haoqi/blob/master/react-baby-v2/ten-dogs/img/dog5.jpg?raw=true'
-      },
-      {
-        id: 6,
-        imgUrl: 'https://github.com/happypeter/demo-haoqi/blob/master/react-baby-v2/ten-dogs/img/dog6.jpg?raw=true'
-      }
-    ]
+    dogs: []
   }
   render () {
     const dogList = this.state.dogs.map(t => (
