7ba042d2ab7787c6fbcd1e44d485bad07a6989f3
Peter Wang <happypeter1983@gmail.com>
2017-09-24 07:08:26 +0800

    dog-9-end

--- react-baby-v2/ten-dogs/src/components/Dog/Dog.js
+++ react-baby-v2/ten-dogs/src/components/Dog/Dog.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
+import './dog.css'
+import axios from 'axios'
 
 class Dog extends Component {
+
+  state = {
+    imgUrl: ''
+  }
+
+  componentDidMount () {
+    const { id } = this.props.match.params
+    axios.get(`http://localhost:3008/dogs/${id}`).then(
+      res => {
+        this.setState({
+          imgUrl: res.data.imgUrl
+        })
+      }
+    )
+  }
   render () {
+    const { imgUrl } = this.state
     return (
       <div className='dog'>
-        小狗详情
+        <img src={imgUrl} alt="dog"/>
       </div>
     )
   }
