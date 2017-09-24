31398ab033b5c96bab5ca04e3885dc920fdae8c6
Peter Wang <happypeter1983@gmail.com>
2017-09-24 15:19:07 +0800

    antd-styled

--- antd-demo/src/components/Home/Home.js
+++ antd-demo/src/components/Home/Home.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
 import LoginForm from '../LoginForm/LoginForm'
+import styled from 'styled-components'
+
+const FormWrap = styled.div`
+  border: 1px solid #ddd;
+  padding: 10px;
+  width: 350px;
+  margin: 100px auto;
+`
 
 class Home extends Component {
   render () {
     return (
       <div className='home'>
-        <LoginForm />
+        <FormWrap>
+          <LoginForm />
+        </FormWrap>
       </div>
     )
   }
