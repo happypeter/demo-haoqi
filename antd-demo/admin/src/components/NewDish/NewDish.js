324e53538fbdfc1d5cf897a18b4b62962f057f0b
Peter Wang <happypeter1983@gmail.com>
2017-09-27 21:43:44 +0800

    antd-form

--- antd-demo/admin/src/components/NewDish/NewDish.js
+++ antd-demo/admin/src/components/NewDish/NewDish.js
@@ -1,13 +1,74 @@
 import React, { Component } from 'react'
+import { Form, Input, Button, Icon, message } from 'antd'
+import styled from 'styled-components'
+import axios from 'axios'
+
+const NewDishWrap = styled.div`
+  max-width: 950px;
+`
 
 class NewDish extends Component {
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    let data = this.props.form.getFieldsValue()
+    let unFilled = Object.keys(data).filter(
+      prop => {
+         return (!data[prop])
+      }
+    )
+    if(unFilled.length === 0 ) {
+      const allData = { ...data,
+        poster: 'https://avatars3.githubusercontent.com/u/72467?v=4&s=460'
+      }
+      axios.post(`http://localhost:3008/dishes`, allData).then(
+        res => {
+          console.log(res.data)
+          this.props.history.push('/dashboard/dishes')
+        }
+      )
+    } else {
+      message.error("请填入全部信息")
+    }
+  }
+
+  hasErrors = (fieldsError) => {
+    return Object.keys(fieldsError).some(field => fieldsError[field]);
+  }
+
   render () {
+    const { getFieldDecorator, getFieldsError } = this.props.form
+    const config = {
+        rules: [{ type: 'string', required: true, message: '必填项目' }],
+      }
     return (
-      <div className='NewDish'>
-        NewDish
-      </div>
+      <NewDishWrap>
+        <Form onSubmit={this.handleSubmit} >
+          <Form.Item>
+            {
+              getFieldDecorator('name', config)(<Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="名称" />)
+            }
+          </Form.Item>
+          <Form.Item>
+            {
+              getFieldDecorator('desc', config)(<Input prefix={<Icon type="tag-o" style={{ fontSize: 13 }} />} placeholder="描述" />)
+            }
+          </Form.Item>
+          <Form.Item>
+            {
+              getFieldDecorator('price', config)(<Input prefix={<Icon type="pay-circle-o" style={{ fontSize: 13 }} />} placeholder="价格" />)
+            }
+          </Form.Item>
+          <Form.Item>
+            <Button disabled={this.hasErrors(getFieldsError())}
+              type='primary' htmlType='submit'>
+              提交
+            </Button>
+          </Form.Item>
+        </Form>
+      </NewDishWrap>
     )
   }
 }
 
-export default NewDish
+export default Form.create()(NewDish)
