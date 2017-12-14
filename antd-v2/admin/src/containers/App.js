3144f8aca77f3e862ab12f5208980e0c517b5e85
Peter Wang <happypeter1983@gmail.com>
2017-12-14 12:49:29 +0800

    rm PrivateRoute

--- antd-v2/admin/src/containers/App.js
+++ antd-v2/admin/src/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import '../assets/global.css'
 import HomeContainer from './HomeContainer'
 import { connect } from 'react-redux'
-import { history, PrivateRoute } from '../utils/routerUtils'
+import { history } from '../utils/routerUtils'
 import {
   Router,
   Switch,
@@ -10,6 +10,7 @@ import {
 } from 'react-router'
 import { loadSelectedIndex } from '../actions/navActions'
 import { fetchDishes } from '../actions/dishActions'
+import DashboardContainer from '../containers/DashboardContainer'
 
 class App extends Component {
   componentDidMount () {
@@ -23,7 +24,7 @@ class App extends Component {
       <Router history={history} >
         <Switch>
           <Route exact path='/' component={HomeContainer} />
-          <PrivateRoute isAuthenticated={isAuthenticated} />
+          <Route path='/dashboard' component={DashboardContainer} />
         </Switch>
       </Router>
     )
--- antd-v2/admin/src/utils/routerUtils.js
+++ antd-v2/admin/src/utils/routerUtils.js
@@ -1,14 +1,3 @@
 import createBrowserHistory from 'history/createBrowserHistory'
-import React from 'react'
-import { Route, Redirect } from 'react-router-dom'
-import DashboardContainer from '../containers/DashboardContainer'
 
 export const history = createBrowserHistory()
-
-export const PrivateRoute = ({ isAuthenticated }) => {
-  if (isAuthenticated) {
-    return <Route path='/dashboard' component={DashboardContainer} />
-  } else {
-    return <Redirect to='/' />
-  }
-}
